c7b10974b4988970310a01412c719adf47814d71
Stefan van der Walt <stefan@sun.ac.za>
2009-01-12 11:32:44 +0200

    Use generator to split tests.

--- scikits/gpu/tests/test_fbo.py
+++ scikits/gpu/tests/test_fbo.py
@@ -3,7 +3,10 @@ from nose.tools import *
 from scikits.gpu.fbo import *
 
 class TestFramebuffer(object):
+    def create(self, x, y, colours):
+        fbo = Framebuffer(x, y, colours)
+
     def test_creation(self):
-        fbo = Framebuffer(16, 16)
+        fbo = Framebuffer(64, 64)
         for bands in range(4):
-            fbo = Framebuffer(16, 16, bands+1)
+            yield self.create, 16, 16, bands+1
